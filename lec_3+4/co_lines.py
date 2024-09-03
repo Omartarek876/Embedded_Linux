7af535f178cd7945f954b3e6163403e2b9200528
omar tarek <ot482002@gmail.com>
2024-09-03 08:56:45 +0300

    update the task to count the no of words in the file too

--- lec_3+4/co_lines.py
+++ lec_3+4/co_lines.py
@@ -1,3 +1,9 @@
 f = open("file.txt" , "r") 
 lines = f.readlines()
-print(len(lines))
+print(len(lines)) 
+f.close()
+
+f = open("file.txt" , "r") 
+words = f.read()
+print(len(words.split()))
+f.close()
