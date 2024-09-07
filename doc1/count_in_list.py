00430fc4027323ba5bed821dc31a424e9d667961
omar tarek <ot482002@gmail.com>
2024-09-07 05:45:33 +0300

    update the task

---
+++ doc1/count_in_list.py
@@ -0,0 +1,3 @@
+list1 = [ 1 , 4 , 6 , 7 , 4]
+
+print(list1.count(4))
---
+++ doc1/vowel_litters.py
@@ -0,0 +1,8 @@
+list1 = [ 'A' , "E" , 'I', 'O' , 'U' , 'a' , 'e' , 'i' , 'o' , 'u']
+
+LITTER = str(input()) 
+if LITTER in list1 :
+   print("vowel")
+else : 
+   print("not vowel")
+
